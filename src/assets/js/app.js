fe1182850a55b9a467a38d4388d41076c6ffddfb
Joao Teodoro <joaotdn@gmail.com>
2016-09-01 17:21:01 -0300

    Completo

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+var menuActiveScroll = function(element) {
+    $(window).on('scroll', function(e) {
+        var posY = e.currentTarget.scrollY;
+        if(posY >= 100)
+            $(element).addClass('active');
+        else
+            $(element).removeClass('active');
+    });
+};
+
+var buttonNextSection = function(element, target) {
+    $(element).on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({scrollTop: $(target).position().top + 'px'},1000);
+    });
+};
+
+(function($) {
+    menuActiveScroll('#home-header');
+    buttonNextSection('.tg-testimonials','#testimonials');
+})(jQuery);
+
